ba71f4f38d870de418e84bdf5163be2d850c0f6b
m <michal@mdevel.net>
2015-03-29 16:28:24 +0200

    removed commented code

--- main.go
+++ main.go
@@ -5,14 +5,6 @@ import "log"
 import "flag"
 import "strconv"
 
-/* var clients *int = flag.Int("c", 1, "Number of clients to simulate") */
-/* var requests *int = flag.Int("n", 1, "Number of requests per client") */
-/* var http_method *string = flag.String("X", "GET", "http method") */
-/* var http_url *string = flag.String("u", "http://example.com", "http url") */
-/* var http_body *string = flag.String("d", "", "http body") */
-/* var http_headers *string = flag.String("H", "", "http headers") */
-/* var ssl_skip *bool = flag.Bool("k", false, "skip ssl cert verification") */
-
 var remote_host *string = flag.String("h", "", "remote host")
 var remote_port *int = flag.Int("p", 3388, "remote port")
 var bind_host *string = flag.String("H", "", "server bind host")
@@ -52,32 +44,4 @@ func main() {
     if *client {
         ServeCtl(os.Stdin, os.Stdout, flood_rpc)
     }
-
-    /* total := *clients * *requests */
-
-    /* fmt.Printf("clients: %d, requests per client: %d, total requests: %d\n", */
-    /*     *clients, *requests, total) */
-
-    /* args := map[string]string{ */
-    /*     "http_method": *http_method, */
-    /*     "http_url": *http_url, */
-    /*     "http_body": body, */
-    /*     "http_headers": *http_headers, */
-    /*     "http_ssl_skip": strconv.FormatBool(*ssl_skip), */
-    /*     "stress_concurrency": strconv.Itoa(*clients), */
-    /*     "stress_iterations": strconv.Itoa(*requests), */
-    /*     "stress_type": "http", */
-    /* } */
-
-    /* reply := &FloodReply{ */
-    /*     reply: make(map[string]string), */
-    /*     peers: nil, */
-    /* } */
-
-    /* stress := NewStress() */
-    /* stress.Run(args, reply) */
-
-    /* fmt.Printf("success: %s, fail: %s, min: %sms, max: %sms, avg: %sms, total test time: %sms\n", */
-    /*     reply.reply["stress_success"], reply.reply["stress_fail"], reply.reply["stress_min_time"], */
-    /*     reply.reply["stress_max_time"], reply.reply["stress_avg_time"], reply.reply["stress_total_time"]) */
 }
